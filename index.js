d87f45e76d4e8932c2a5e3dd1fb2beaacbc86735
Shiya Luo <shiya.luo@lever.co>
2019-08-17 16:17:24 -0700

    Append tweets

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const path = require('path')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+let tweets = [];
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+app.post('/tweet', (req, res) => {
+  tweets.push(req.body.tweet)
+  res.redirect('/')
+})
+
+app.get('/tweets', (req, res) => {
+  res.send(tweets);
 })
 
 app.use(express.static('public'))
